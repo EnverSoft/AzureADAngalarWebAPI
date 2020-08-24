62bf2f4680570a203e28c3d0256ded6402f1e645
Werner Bisschoff <werner.bisschoff@daimler.com>
2020-08-24 08:43:51 +0200

    Initial Commit

---
+++ AzureMSALAngular/src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  baseUrl:'http://localhost:58980/',
+  scopeUri: ['api://2b1aad4e-80ba-4be2-81e0-02bf3904ca6f/apportal'],
+  tenantId: '2015224f-85d0-40ef-939a-6e2a244eb681',
+  uiClienId: '2b1aad4e-80ba-4be2-81e0-02bf3904ca6f',
+  redirectUrl: 'http://localhost:4200'
+};
